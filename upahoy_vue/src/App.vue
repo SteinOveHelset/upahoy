8478be1fdecf547b443aa04a7b95bde086f82ae9
Code With Stein <codewithstein@gmail.com>
2022-07-31 18:51:30 +0200

    first commit

---
+++ upahoy_vue/src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <nav class="py-6 px-8 bg-gray-800 flex items-center justify-between text-white">
+      <div class="menu-left">
+        <a href="/" class="text-xl text-white">Upahoy</a>
+      </div>
+
+      <div class="menu-right space-x-4">
+        <a href="#" class="px-4 py-2 bg-cyan-400 rounded-xl">Sign up</a>
+        <a href="#" class="px-4 py-2 bg-gray-600 rounded-xl">Log in</a>
+      </div>
+    </nav>
+
+    <main>
+      <RouterView />
+    </main>
+
+    <footer class="py-12 px-8 flex justify-between bg-gray-800 text-white">
+      <div class="footer-left">
+        <h3 class="mb-5 text-xl">Upahoy</h3>
+
+        <p class="text-sm">
+          Lorem ipsum bla bal bla orem ipsum bla bal bla orem ipsum bla bal bla
+        </p>
+      </div>
+
+      <div class="w-1/2 footer-right">
+        <h3 class="mb-5 text-xl">Menu</h3>
+
+        <div class="flex flex-col space-y-4">
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+          <a href="#">Terms of service</a>
+          <a href="#">Privacy policy</a>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ upahoy_vue/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <div class="hero bg-cyan-800">
+      <div class="max-w-4xl mx-auto py-16 text-center">
+        <h1 class="mb-8 text-5xl text-white">Welcome to Upahoy</h1>
+
+        <p class="mb-8 text-2xl text-white">Lorem ipsum bla bal bla orem ipsum bla bal bla orem ipsum bla bal bla</p>
+
+        <a href="#" class="px-6 py-4 inline-block bg-white rounded-xl hover:bg-gray-300">Try for free</a>
+      </div>
+    </div>
+  </main>
+</template>
